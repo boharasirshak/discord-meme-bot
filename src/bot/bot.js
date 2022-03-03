f1a9f15f67591919fde892bcc329c26a37a9a216
Sirshak Bohara <021neb459@sxc.edu.np>
2022-03-03 20:00:48 +0545

    init

---
+++ src/bot/bot.js
@@ -0,0 +1,11 @@
+// const { Client } = require('discord.js');
+const helper = require('./util/helper');
+// const client = new Client();
+
+// client.login();
+
+// client.on('ready', () => {
+//     console.log(`Logged in as ${client.user.tag}!`);
+// });
+
+helper.readSettings();
