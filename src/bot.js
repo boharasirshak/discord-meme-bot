6329bc461052b5899845e014c3d5bba8a31dca3f
Sirshak Bohara <021neb459@sxc.edu.np>
2022-03-03 21:04:12 +0545

    init

--- src/bot.js
+++ src/bot.js
@@ -8,9 +8,22 @@ if (!env.isVaild()) {
 }
 
 const botToken = env.botToken;
+const prefix = env.prefix;
 
 client.login(botToken);
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', (message) => {
+
+    if (message.author.bot) return;
+    if (!message.content.startsWith(prefix)) return;
+
+    let body = message.content.slice(prefix.length);
+    let args = body.split(" ");
+    let command = prefix + args.shift().toLowerCase();
+    let user = "@" + message.author.username + "#" + message.author.discriminator;
+
 });
--- src/util/envparser.js
+++ src/util/envparser.js
@@ -1,10 +1,15 @@
 require('dotenv').config()
 
 const botToken = process.env.DISCORD_BOT_TOKEN || undefined;
+const prefix = process.env.PREFIX || undefined;
 
 
 module.exports.isVaild = function () {
-    return botToken ? true : false;
+    if (!botToken) return false;
+    if (!prefix) return false;
+
+    return true;
 }
 
 module.exports.botToken = botToken;
+module.exports.prefix = prefix;
