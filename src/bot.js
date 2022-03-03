1662dfde4ce57fd79579822717cf8d618246d71d
Sirshak Bohara <021neb459@sxc.edu.np>
2022-03-03 21:47:02 +0545

    added cmd commands

--- src/bot.js
+++ src/bot.js
@@ -1,5 +1,6 @@
 const { Client } = require('discord.js');
 const env = require('./util/envparser');
+const { sendMessage, getCommands } = require('./commands/message');
 const client = new Client({ intents: ['GUILDS', 'GUILD_MESSAGES'] });
 require('dotenv').config()
 
@@ -16,14 +17,27 @@ client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', (message) => {
+client.on('messageCreate', message => {
 
     if (message.author.bot) return;
     if (!message.content.startsWith(prefix)) return;
 
     let body = message.content.slice(prefix.length);
     let args = body.split(" ");
-    let command = prefix + args.shift().toLowerCase();
+    let command = args.shift().toLowerCase();
     let user = "@" + message.author.username + "#" + message.author.discriminator;
+
+    if (command === "cmd") {
+        sendMessage(
+            message,
+            "My Commands",
+            "#FFA500",
+            `Here are all my **commands** :- \n ${getCommands()}`
+        );
+
+    }
+    else {
+        console.log("No Prefix");
+    }
 
 });
